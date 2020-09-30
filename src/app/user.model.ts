89ad1230937d5d1169e94f5a8b6754f798d20d89
EPHMRS <spssps99@gmail.com>
2020-09-30 17:02:59 +0530

    added login model

---
+++ src/app/user.model.ts
@@ -0,0 +1,14 @@
+export class User{
+    
+    public name:string;
+    public dob:string;
+    public email:string;
+    public phone: string; 
+    public uname:string;
+    public password:string;
+    public address:string;
+    public card:string;
+    public bank:string;
+    public account:string;
+    public ifsc:string;
+}
