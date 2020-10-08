f42fceec1554af080cb4c3dedbf77cac852687c4
EPHMRS <spssps99@gmail.com>
2020-10-08 10:21:15 +0530

    ts changes

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,8 @@ import { Emicard } from '../emicard.model';
 import {Register} from '../register.model';
 import { AdmintableService } from '../services/admintable.service';
 import { EmicardService } from '../services/emicard.service';
+import { DatePipe, formatDate } from '@angular/common';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -13,8 +15,16 @@ export class RegisterComponent implements OnInit {
  register=new Register;
  card=new Emicard;
  banks:string[];
+ myDate = new Date();
+ exDate = new Date(this.myDate.getFullYear()+4,this.myDate.getMonth(),this.myDate.getDate())
+ jstoday=''
+ jexpiry=''
    constructor(private router:Router,private service: AdmintableService,private service2: EmicardService) { 
     this.banks=['HDFC','Kotak','ICICI','SBI'];
+    this.jstoday = formatDate(this.myDate, 'MM-yyyy', 'en-US', '+0530');
+    this.jexpiry = formatDate(this.exDate, 'MM-yyyy', 'en-US', '+0530' )
+    console.log(this.jstoday);
+    console.log(this.jexpiry);
    }
 
   ngOnInit(): void {
@@ -22,9 +32,9 @@ export class RegisterComponent implements OnInit {
 savePerson(){
   if(this.register.password===this.register.cpassword){
   this.card.user=this.register;
-  this.card.issueDate="02/20";
-  this.card.expiryDate="04/24";
-  if(this.register.card=="gold"){
+  this.card.issueDate=this.jstoday;
+  this.card.expiryDate=this.jexpiry;
+  if(this.register.card=="Gold"){
     this.card.balance=200000;
   }
   else{
